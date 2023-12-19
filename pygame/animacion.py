67e56884f627919f3fae893830704f419db41f9c
emanuelbe1 <emanuelbe1@gmail.com>
2023-12-18 23:31:27 -0300

    nuevo archivo

---
+++ pygame/animacion.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 18 22:53:19 2023
+
+@author: emanuel
+"""
+
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import numpy as np
+import pygame
+
+
+pygame.init()
+pygame.mixer.music.load('fur_elise_segunda_may_asc.ogg')
